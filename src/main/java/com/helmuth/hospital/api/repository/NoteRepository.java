e1a1be51c7d6774ec189693c7a1b6de54e6560b7
helmuth <helmuth.hernandez.alcazar@alumnos.upm.es>
2023-04-21 17:02:58 +0200

    Notes find by patient

--- src/main/java/com/helmuth/hospital/api/repository/NoteRepository.java
+++ src/main/java/com/helmuth/hospital/api/repository/NoteRepository.java
@@ -1,12 +1,18 @@
 package com.helmuth.hospital.api.repository;
 
 import com.helmuth.hospital.api.entity.Note;
+import com.helmuth.hospital.api.entity.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
 @RepositoryRestResource
 @CrossOrigin("*")
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    Page<Note> findByPatient(Pageable pageable, Patient patient);
 }
